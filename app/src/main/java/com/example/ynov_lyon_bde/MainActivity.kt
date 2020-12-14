a9c39df823de59354b3f504c39f4bba12894f4cd
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2020-12-14 15:09:50 +0100

    Add onClickListener in card

--- app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.ynov_lyon_bde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        val host = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, host).setPrimaryNavigationFragment(host).commit()
     }
 }
