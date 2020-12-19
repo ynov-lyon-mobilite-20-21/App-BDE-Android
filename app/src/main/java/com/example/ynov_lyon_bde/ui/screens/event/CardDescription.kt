689006353d411cb64b4cc3ac175d839b96ad3317
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2020-12-19 18:01:35 +0100

    UI card description finish

--- app/src/main/java/com/example/ynov_lyon_bde/ui/screens/event/CardDescription.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/ui/screens/event/CardDescription.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.ynov_lyon_bde.R
 import kotlinx.android.synthetic.main.fragment_card_description.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class CardDescription : Fragment() {
 
@@ -18,7 +19,7 @@ class CardDescription : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_card_description, container, false)
 
-        view.button1.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_card_description_to_homeFragment)}
+        view.back_home_fragment.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_card_description_to_homeFragment) }
 
         return view
     }
