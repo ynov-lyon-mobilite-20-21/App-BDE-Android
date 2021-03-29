19374878171c2196ebff49c493798180efe0df69
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2021-03-29 16:10:58 +0200

    feat: ScrollView OK inside edit informations profile fragement view

--- app/src/main/java/com/example/ynov_lyon_bde/ui/screens/profil/PersonalInformationsUserFragment.kt
+++ app/src/main/java/com/example/ynov_lyon_bde/ui/screens/profil/PersonalInformationsUserFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.ynov_lyon_bde.R
-import kotlinx.android.synthetic.main.fragment_edit_informations_profile.view.*
+import kotlinx.android.synthetic.main.fragment_personal_informations_user.view.*
 import kotlinx.android.synthetic.main.fragment_settings_user_profile.view.*
 import kotlinx.android.synthetic.main.fragment_settings_user_profile.view.back_icon
 
